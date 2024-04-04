773bd62ec4198337d9279a8adb64603cf35b3a8b
PiMedina <pmedinaangulo@frba.utn.edu.ar>
2024-04-04 01:54:28 +0000

    vinculando con utils

--- kernel/settings.mk
+++ kernel/settings.mk
@@ -1,8 +1,8 @@
 # Libraries
-LIBS=commons pthread readline m
+LIBS=utils commons pthread readline m
 
 # Custom libraries' paths
-STATIC_LIBPATHS=
+STATIC_LIBPATHS=../utils
 
 # Compiler flags
 CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
--- memoria/src/main.c
+++ memoria/src/main.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <commons/txt.h>
 
+#include <utils/hello.h>
+
 int main(int argc, char* argv[]) {
     txt_write_in_stdout("Hola, Operativos!!\n");
+
+    say_hello("Hola desde la libreria estatica");
+
     return 0;
 }
